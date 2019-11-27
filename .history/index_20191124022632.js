d0da99a6bb26ecf0a0d40100bc7a5c8d415aa6a7
CristianPalta1 <cristian.palta.c@uniautonoma.edu.co>
2019-11-27 13:39:42 -0500

    Primer Commit

---
+++ .history/index_20191124022632.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.status(200).;
+    res.send('Hola :D');
+  });
+  
+app.listen(3000, function () {
+    console.log('Estoy Funcionando ing.');
+  });
---
+++ .history/index_20191124022714.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.status(200).send ('Hola :D');
+  });
+  
+app.listen(3000, function () {
+    console.log('Estoy Funcionando ing.');
+  });
